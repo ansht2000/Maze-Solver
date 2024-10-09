0db947a28ad78bb61be2f6a041a41a483218426a
Ansh Tarafder <ansht2000@gmail.com>
2024-10-08 18:06:54 -0700

    implemented drawing lines in the window

--- graphics.py
+++ graphics.py
@@ -2,7 +2,7 @@ from tkinter import Tk, BOTH, Canvas
 
 
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
@@ -22,3 +22,33 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line: "Line", fill_color: str):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x: int, y: int):
+        self.__x = x
+        self.__y = y
+
+    def __repr__(self):
+        return f"({self.__x}, {self.__y})"
+    
+    def get_x(self):
+        return self.__x
+    
+    def get_y(self):
+        return self.__y
+
+class Line:
+    def __init__(self, start_point: Point, end_point: Point):
+        self.__start = start_point
+        self.__end = end_point
+
+    def __repr__(self):
+        return f"{self.__start} -> {self.__end}"
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.__start.get_x(), self.__start.get_y(), self.__end.get_x(), self.__end.get_y(), fill = fill_color, width = 2
+        )
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from graphics import Window
+from graphics import *
 
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(0, 0), Point(300, 300))
+    line_2 = Line(Point(800, 600), Point(300, 300))
+    win.draw_line(line=line, fill_color="black")
+    win.draw_line(line=line_2, fill_color="red")
     win.wait_for_close()
 
 main()
